c6fa5c2a166f2a07b2803f02ed6bce7938f60632
Natalia-Armond <taya.armond@gmail.com>
2025-11-17 15:42:33 -0300

    Adiciona exercício com arrays

---
+++ arrays.js
@@ -0,0 +1,8 @@
+/* Tarefa de Código dia 5
+Manipulação de Arrays
+CONTEXTO: Aprender a manipular arrays em JavaScript.Essencial para gerenciar listas de dados.TEMPO: 35min PASSOS: 1. Crie um arquivo `arrays.js` 2. Cole: ```js let frutas = ['maçã', 'banana', 'laranja']; frutas.push('uva'); console.log(fruitas); ``` 3. Terminal: `node arrays.js` Esperado: `['maçã', 'banana', 'laranja', 'uva']` ✅ CONCLUÍDO: Código roda e retorna a lista atualizada.*/
+
+
+let frutas = ['maçã', 'banana', 'laranja'];
+frutas.push('uva');
+console.log(frutas);
